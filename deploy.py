83f382550409c7f61684a371cee9bfe28e230a33
Alexey <doc.x@mail.ru>
2012-08-28 13:53:52 +0300

    first version deploy.py

--- deploy.py
+++ deploy.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
 
+import sys
+import os
+#import shutil 
+
 files = {
 	'bashrc' : '~/.bashrc',
-	'inoutrc' : '~/.inputrc',
+	'inputrc' : '~/.inputrc',
 	'profile' : '~/.profile',	
 }
+
+
+cur_dir = os.path.dirname(sys.argv[0])
+
+for file in files:
+	move_path = files[file]	
+	file = cur_dir+'/data/'+file	
+	os.system('cp '+file+' '+move_path+'')
